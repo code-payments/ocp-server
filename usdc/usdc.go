2822c63e7cd4427666b2b35eb911b73ee3a216c2
jeffyanta <jeff@flipcash.com>
2026-05-14 13:55:03 -0400

    GetTokenAccountInfos now returns the USDC ATA (#208)

--- usdc/usdc.go
+++ usdc/usdc.go
@@ -1,7 +1,11 @@
 package usdc
 
 const (
-	Mint          = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+	Name   = "USDC"
+	Symbol = "USDC"
+
+	Mint = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+
 	QuarksPerUsdc = 1000000
 	Decimals      = 6
 )
