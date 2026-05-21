ce48a0a0111c5d9ea8c1cd7335bcc5a8025a4bb0
jeffyanta <jeff@flipcash.com>
2026-05-21 12:08:19 -0400

    Add ability to notify of stateless swap being submitted

--- ocp/integration/swap.go
+++ ocp/integration/swap.go
@@ -9,6 +9,9 @@ import (
 
 // Swap is an integration that hooks into the swap worker
 type Swap interface {
+	// OnSwapSubmitted allows for notification when the swap for when a swap is submitted
+	OnSwapSubmitted(ctx context.Context, owner *common.Account, fromMint, toMint *common.Account) error
+
 	// OnSwapFinalized allows for notifications based on events processed by the swap worker
 	OnSwapFinalized(ctx context.Context, owner *common.Account, isBuy bool, mint *common.Account, currencyName string, region currency.Code, valueReceived float64, isMintInit bool) error
 }
