05a57a9d3ee5f5be6de7647501afe04aaa9bb6b3
jeffyanta <jeff@flipcash.com>
2026-09-10 14:38:51 -0400

    Update to original Coinbase Stableswapper swap ixn format

--- solana/coinbasestableswapper/types_account_type.go
+++ solana/coinbasestableswapper/types_account_type.go
@@ -5,6 +5,9 @@ var (
 	// LiquidityPool discriminator: [66, 38, 17, 64, 188, 80, 68, 129]
 	LiquidityPoolAccountDiscriminator = []byte{66, 38, 17, 64, 188, 80, 68, 129}
 
+	// AddressWhitelist discriminator: [10, 102, 46, 176, 154, 249, 160, 48]
+	AddressWhitelistAccountDiscriminator = []byte{10, 102, 46, 176, 154, 249, 160, 48}
+
 	// TokenVault discriminator: [121, 7, 84, 254, 151, 228, 43, 144]
 	TokenVaultAccountDiscriminator = []byte{121, 7, 84, 254, 151, 228, 43, 144}
 )
